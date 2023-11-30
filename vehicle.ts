b65bbd953c6994e9a6a43fb7f7c9e170b5da1a2f
Deivyson José da Silva <139878479+Deivyson1401@users.noreply.github.com>
2023-11-30 02:25:14 +0000

    atualização

--- vehicle.ts
+++ vehicle.ts
@@ -1,6 +1,11 @@
 class vehicle {
+    modelo: string
+    cor: string
+    numeroRodas: number
     constructor (private modelo: string, cor: string, numeroRodas: number){
-
+        this.modelo = modelo
+        this.cor = cor
+        this.numeroRodas = numeroRodas
     }
 
     private clone(): void {}
@@ -10,4 +15,17 @@ class vehicle {
 }
 
 
-class Car1 extends vehicle {}
+class Car1 extends vehicle {
+    constructor (modelo:string, cor: string, numeroRodas:number, numeroPortas: number){
+        super(modelo,cor,numeroRodas)
+    }
+}
+
+class car2 extends vehicle {
+
+}
+
+const name = 'ola'
+name = 'oi'
+
+console.log(name);

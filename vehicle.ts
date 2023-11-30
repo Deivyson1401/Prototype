a80310b9aa7469a2bbe7f9c795bfb3ae532e511a
Deivyson José da Silva <139878479+Deivyson1401@users.noreply.github.com>
2023-11-30 02:08:32 +0000

    initial commit

---
+++ vehicle.ts
@@ -0,0 +1,13 @@
+class vehicle {
+    constructor (private modelo: string, cor: string, numeroRodas: number){
+
+    }
+
+    private clone(): void {}
+    
+    private represent(): void {}
+
+}
+
+
+class Car1 extends vehicle {}
